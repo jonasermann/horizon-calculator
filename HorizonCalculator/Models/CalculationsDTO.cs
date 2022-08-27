db6146ea60e64b888ec71f11e85339cde569bb28
Jonas Ermann <jonas.ermann@hotmail.com>
2022-08-27 13:27:17 +0200

    Add CalculationsDTO

---
+++ HorizonCalculator/Models/CalculationsDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace HorizonCalculator.Models
+{
+    public class CalculationsDTO
+    {
+        public double ObserverHeight { get; set; }
+
+        public double ObjectHeight { get; set; }
+
+        public double ObserverObjectGeographicalDistance { get; set; }
+    }
+}
