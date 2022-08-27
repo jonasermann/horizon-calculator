d4aa5decbeba6e986ae82d8a9b09a6924888396f
Jonas Ermann <jonas.ermann@hotmail.com>
2022-08-27 13:58:05 +0200

    Add Functionality

--- HorizonCalculator/Controllers/HomeController.cs
+++ HorizonCalculator/Controllers/HomeController.cs
@@ -6,8 +6,11 @@ namespace HorizonCalculator.Controllers;
 
 public class HomeController : Controller
 {
-    public ActionResult<Calculations> Index()
+    public ActionResult<Calculations> Index(double ObserverHeight, double ObjectHeight, double ObserverObjectGeographicalDistance) => View(new Calculations
     {
-        return View();
-    }
+        Radius = 6371,
+        ObserverHeight = ObserverHeight,
+        ObjectHeight = ObjectHeight,
+        ObserverObjectGeographicalDistance = ObserverObjectGeographicalDistance
+    });
 }
