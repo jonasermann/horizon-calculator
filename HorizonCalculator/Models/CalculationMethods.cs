e83734174dc588b560b11bed600560dc1058a05e
Jonas Ermann <jonas.ermann@hotmail.com>
2022-09-14 14:53:44 +0200

    Add CalculationMethods File

---
+++ HorizonCalculator/Models/CalculationMethods.cs
@@ -0,0 +1,44 @@
+﻿namespace HorizonCalculator.Models;
+
+public static class CalculationMethods
+{
+    public static double GetVerticalDrop(double radius, double height)
+    {
+        return height * radius / (height + radius);
+    }
+
+    public static double GetHorizontalDrop(double radius, double height, double verticalDrop)
+    {
+        return verticalDrop * Math.Sqrt(1 + (2 * radius / height));
+    }
+
+    public static double GetAngle(double radius, double horizontalDrop)
+    {
+        return Math.Asin(horizontalDrop / radius);
+    }
+
+    public static double GetHorizonGeographicalDistance(double radius, double angle)
+    {
+        return radius * angle;
+    }
+
+    public static double GetAngleFromHorizonGeographicalDistance(double radius, double horizonGeographicalDistance)
+    {
+        return horizonGeographicalDistance / radius;
+    }
+
+    public static double GetHorizontalDropFromAngle(double radius, double angle)
+    {
+        return Math.Sin(angle) * radius;
+    }
+
+    public static double GetVerticalDropFromHorizontalDrop(double radius, double horizontalDrop)
+    {
+        return radius - Math.Sqrt(Math.Pow(radius, 2) - Math.Pow(horizontalDrop, 2));
+    }
+
+    public static double GetHeightFromVerticalDrop(double radius, double verticalDrop)
+    {
+        return (radius * verticalDrop) / (radius - verticalDrop);
+    }
+}
--- HorizonCalculator/Models/Calculations.cs
+++ HorizonCalculator/Models/Calculations.cs
@@ -1,3 +1,5 @@
+using static HorizonCalculator.Models.CalculationMethods;
+
 namespace HorizonCalculator.Models;
 
 public class Calculations
@@ -10,13 +12,11 @@ public class Calculations
 
     public double ObserverObjectGeographicalDistance { get; set; }
 
-
-
     public double ObserverHeightInKm => ObserverHeight / 1000;
 
     public double ObjectHeightInKm => ObjectHeight / 1000;
 
-    public double ObserverVerticalDrop => GetVerticalDrop(Radius, ObserverHeightInKm); 
+    public double ObserverVerticalDrop => GetVerticalDrop(Radius, ObserverHeightInKm);
 
     public double ObserverHorizontalDrop => GetHorizontalDrop(Radius, ObserverHeightInKm, ObserverVerticalDrop);
 
@@ -32,46 +32,4 @@ public class Calculations
 
     public double VisibleObjectHeight => Math.Round((ObjectHeightInKm - ReducedObjectHeight) * 1000, 2);
 
-
-
-
-    public static double GetVerticalDrop(double radius, double height)
-    {
-        return height * radius / (height + radius);
-    }
-
-    public static double GetHorizontalDrop(double radius, double height, double verticalDrop)
-    {
-        return verticalDrop * Math.Sqrt(1 + (2 * radius / height));
-    }
-
-    public static double GetAngle(double radius, double horizontalDrop)
-    {
-        return Math.Asin(horizontalDrop / radius);
-    }
-
-    public static double GetHorizonGeographicalDistance(double radius, double angle)
-    {
-        return radius * angle;
-    }
-
-    public static double GetAngleFromHorizonGeographicalDistance(double radius, double horizonGeographicalDistance)
-    {
-        return horizonGeographicalDistance / radius;
-    }
-
-    public static double GetHorizontalDropFromAngle(double radius, double angle)
-    {
-        return Math.Sin(angle) * radius;
-    }
-
-    public static double GetVerticalDropFromHorizontalDrop(double radius, double horizontalDrop)
-    {
-        return radius - Math.Sqrt(Math.Pow(radius, 2) - Math.Pow(horizontalDrop, 2));
-    }
-
-    public static double GetHeightFromVerticalDrop(double radius, double verticalDrop)
-    {
-        return (radius * verticalDrop) / (radius - verticalDrop);
-    }
 }
